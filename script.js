7277fa43e3e057d228f98421538e3d561b8b876b
goonruntongue <68429888+goonruntongue@users.noreply.github.com>
2024-03-28 01:33:27 +0900

    コピー機能の追加

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ $(document).ready(() => {
         url: "data.json",
         success: (data) => {
             $.each(data, (key, value) => {
-                $("pre").append(key + "\n" + value + "\n\n");
+                $("pre").append(key + "<span class='copy'></span>" + "\n" + value + "\n\n");
             });
             // AJAXコールバックの成功後にGoogle Code Prettifyを動的にロード
             setTimeout(() => {
                 $.getScript("https://cdn.jsdelivr.net/gh/google/code-prettify/loader/run_prettify.js?skin=sons-of-obsidian");
+                $(".copy").each((index, elem) => {
+                    $(elem).on("click", function() {
+                        let copiedText = $(this).closest("li").text();
+                        console.log(copiedText);
+                        navigator.clipboard.writeText(copiedText);
+                        $(elem).addClass("on").delay(1000).queue(next => {
+                            $(elem).removeClass("on")
+                            next();
+                        })
+                    })
+                })
             }, 10);
         }
     });
